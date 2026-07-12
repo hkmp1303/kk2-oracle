5b33cdea1242178087f2728722f36b80d1869992
Heather <101600223+hkmp1303@users.noreply.github.com>
2026-07-12 10:34:35 +0200

    Add runnable.py to support chaining pipeline sequences following example
    Abstract class that facilitates inheritance of chainability to other
    classes

---
+++ app/chain/runnable.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel, ConfigDict, SerializeAsAny
+from typing import Any, Callable, Generic, TypeVar
+
+I = TypeVar("I")
+O = TypeVar("O")
+M = TypeVar("M")
+
+# Glue code
+class Runnable(BaseModel, Generic[I, O]):
+
+    name: str | None = None
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    def invoke(self, data: I) -> O:
+        raise NotImplementedError("Subclass is not implemented")
+    def __or__(self, other: Any) -> RunnableSequence:
+        if isinstance(other, Runnable):
+            return RunnableSequence.model_construct(
+                first=self,
+                second=other
+            )
+        if callable(other):
+            return RunnableSequence.model_construct(
+                first=self,
+                second=RunnableLambda.model_construct(func=other, name=other.__name__),
+                name=other.__name__
+            )
+        return NotImplemented
+
+    def __ror__(self, other: Any) -> Any:
+        if callable(other):
+            return RunnableSequence.model_construct(
+                first=RunnableLambda.model_construct(func=other),
+                second=self,
+                name=other.__name__
+            )
+        return NotImplemented
+
+class RunnableLambda(Runnable[I, O]):
+    func: Callable[[I], O]
+
+    def invoke(self, data: I) -> O:
+        return self.func(data)
+
+class RunnableSequence(Runnable[I, O], Generic[I, M, O]):
+    first: SerializeAsAny[Runnable[I, M]]
+    second: SerializeAsAny[Runnable[M, O]]
+
+    def invoke(self, data: I) -> O:
+        return self.second.invoke(self.first.invoke(data))
